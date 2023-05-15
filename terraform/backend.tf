20dce70467693619a5326c89724f2f1d0d7a9754
estehsan <estehsaan@gmail.com>
2023-05-15 08:21:29 +0200

    terraform updated 2

---
+++ terraform/backend.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source                = "hashicorp/azurerm"
+      version               = ">= 3.30"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}  
+}
+
+terraform {
+  backend "azurerm" {
+    storage_account_name = "azurewebapptaskdv"
+    container_name       = "tfbackend"
+    key                  = "test.state"
+    subscription_id      = "fb121715-e9af-41d9-b113-aae88bed3c6d"
+    tenant_id            = "ffa63cad-6366-428d-97c3-019bd2ba9d93"
+    resource_group_name  = "azurenodejsapplication-rg"
+  }
+}
