363e65988f4e04491513b1c80289ae82b96642e8
Estehsan <games.huseb@gmail.com>
2023-05-16 17:46:51 +0000

    Storage name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -14,7 +14,7 @@ provider "azurerm" {
 terraform {
   backend "azurerm" {
     storage_account_name = "azurewebapptaskdv"
-    container_name       = "tfbackend"
+    container_name       = "acr15may"
     key                  = "test.state"
     subscription_id      = "fb121715-e9af-41d9-b113-aae88bed3c6d"
     tenant_id            = "ffa63cad-6366-428d-97c3-019bd2ba9d93"
