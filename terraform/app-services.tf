f6aee3363961120144881c52e7b9848b0123bf81
estehsan <estehsaan@gmail.com>
2023-05-17 23:12:42 +0200

    update git

--- terraform/app-services.tf
+++ terraform/app-services.tf
@@ -1,26 +1,29 @@
 ###########################################################
 # Azure App Services
 ###########################################################
+
 variable "app_name" {
   default = "aznodejsapp"
 }
 
-# Set the default environment to dev
-variable "app_environment" {
+variable "resource_group_name" {
+  default = "aznodejsapp-rg"
+}
+
+variable "environment" {
   default = "dev"
 }
 
 # Create a Resource Group
 
-resource "azurerm_resource_group" "tf_rg" {
-  name = "aznodejsapp-rg"
-  location = "West Europe"
+data "azurerm_resource_group" "tf_rg" {
+  name = var.resource_group_name
 }
 
 # Create an App Service Plan
 
 resource "azurerm_app_service_plan" "tf_app_service_plan" {
-  name                = "${var.app_name}-${var.app_environment}-app-service-plan-rg"
+  name                = "${var.app_name}-${var.environment}-app-service-plan"
   location            = data.azurerm_resource_group.tf_rg.location
   resource_group_name = data.azurerm_resource_group.tf_rg.name
   kind                = "Linux"
@@ -35,13 +38,13 @@ resource "azurerm_app_service_plan" "tf_app_service_plan" {
 # Create an App Service
 
 resource "azurerm_app_service" "tf_app_service" {
-  name                = "${var.app_name}-${var.app_environment}-service"
+  name                = "${var.app_name}-${var.environment}-app-service"
   location            = data.azurerm_resource_group.tf_rg.location
   resource_group_name = data.azurerm_resource_group.tf_rg.name
   app_service_plan_id = azurerm_app_service_plan.tf_app_service_plan.id
 
   tags = {
     Name        = "${var.app_name}-app-service"
-    Environment = var.app_environment
+    Environment = var.environment
   }
 }
