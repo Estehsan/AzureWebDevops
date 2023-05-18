3e84f1606162d65cea27bbf152dadc7a3354a6eb
Estehsan <games.huseb@gmail.com>
2023-05-18 07:49:07 +0000

    update

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
 }
 
 
-#Terraform code to create a resource group.
+#Terraform code to create a resource group..
 terraform {
   backend "azurerm" {
     storage_account_name = "azurewebapptaskdv"
